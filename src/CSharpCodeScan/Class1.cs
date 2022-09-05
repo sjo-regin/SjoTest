4e91c85a20adf2c684db6181e17fba0d3e3b7bc5
Stefan Johansson <stefan.johansson@regin.se>
2022-09-05 08:30:12 +0200

    Test CodeScan

---
+++ src/CSharpCodeScan/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeScan
+{
+    class Class1
+    {
+
+        int[] intArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public void Test1()
+        {
+
+            int nr = 10;
+            for (int i = 0; i < nr; i++)
+            {
+
+                Trace.WriteLine($"Test1() intArray[{i}] = {intArray[i]}");
+
+            }
+        }
+
+        public void Test2()
+        {
+
+            int nr = 15;
+            for (int i = 0; i < nr; i++)
+            {
+
+                Trace.WriteLine($"Test2() intArray[{i}] = {intArray[i]}");
+
+            }
+        }
+
+        public void Test3()
+        {
+
+            int nr = 15;
+            for (int i = 0; i < nr; i++)
+            {
+
+                Trace.WriteLine($"Test3() intArray2[{i}] = {intArray2[i]}");
+
+            }
+        }
+    }
+}
